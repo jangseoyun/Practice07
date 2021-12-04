468e82faaa4b608c160cb1fff7e3fb411b7f731c
장서윤 <may-primavera@naver.com>
2021-12-04 14:32:37 +0900

    Practice07 추상클래스 / 인터페이스

---
+++ src/com/javaex/ex01/Ractangle.java
@@ -0,0 +1,29 @@
+package com.javaex.ex01;
+
+public class Ractangle extends Shape{
+	
+	private int width;
+	private int height;
+	
+	public Ractangle(String fillColor, int width, int height){
+		super(fillColor);
+		this.width=width;
+		this.height=height;
+	}
+	
+
+	public void draw(){
+		System.out.println("[사각형]#면색:" +super.fillColor+ " "
+				+ " #가로:" + width 
+				+" #세로:" + height + "그렸습니다.");                                                                        
+	}
+	
+	public void srwidth() {
+		System.out.println("sr1의 가로는 "+width+" 입니다.");
+	}
+
+	
+
+}
+
+
